3557c332b64d71833f3a0847fa293541c8d34e5e
Ismoil4432 <ismoil4432@gmail.com>
2023-06-02 01:48:21 +0500

    home page, component for posts added, default layout designed

---
+++ components/Sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+  <aside class="h-screen w-[250px] bg-slate-200 fixed top-0">
+    <h1>logo</h1>
+
+    <ul>
+      <li>
+        <NuxtLink to="/">Home</NuxtLink>
+      </li>
+    </ul>
+  </aside>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
