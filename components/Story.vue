e45b36f677e9b96467d5d34e62887f94cd0e3066
Yoqubjon-Dev <yoqubjonyoqubjonov4@gmail.com>
2023-06-02 15:35:10 +0500

    Story.vue-created

---
+++ components/Story.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex gap-[20px] ml-[100px] mt-[100px]">
+    <div class="flex flex-col items-center">
+      <img class="w-[56px] h-[56px] rounded-full" src="../public/images/story-profile-1.png" alt="">
+      <p class="text-[12px] text-center">itsdfghjou</p>
+    </div>
+    <div class="flex flex-col items-center">
+      <img class="w-[56px] h-[56px] rounded-full" src="../public/images/story-profile-2.png" alt="">
+      <p class="text-[12px] text-center">openaidalle</p>
+    </div>
+    <div class="flex flex-col items-center">
+      <img class="w-[56px] h-[56px] rounded-full" src="../public/images/story-profile-3.png" alt="">
+      <p class="text-[12px] text-center">lewishamilton</p>
+    </div>
+    <div class="flex flex-col items-center">
+      <img class="w-[56px] h-[56px] rounded-full" src="../public/images/story-profile-4.png" alt="">
+      <p class="text-[12px] text-center">wahab.xyz</p>
+    </div>
+    <div class="flex flex-col items-center">
+      <img class="w-[56px] h-[56px] rounded-full" src="../public/images/story-profile-5.png" alt="">
+      <p class="text-[12px] text-center">defavours</p>
+    </div>
+    <div class="flex flex-col items-center">
+      <img class="w-[56px] h-[56px] rounded-full" src="../public/images/story-profile-6.png" alt="">
+      <p class="text-[12px] text-center">mkbhd</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">Hello world! </h1>
-    <h2>kmdueowi</h2>
-    <h3>456789</h3>
-    <h1>oweu33453</h1>
-    <h2>ghjk</h2>
+    <Story/>
   </div>
 </template>
 
