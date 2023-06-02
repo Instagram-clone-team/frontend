67f73ab68aa52d077e3602bc62b0b1086a61fd02
Abukarimov <abdukarimovmuslimbek2004@gmail.com>
2023-06-02 11:44:50 +0500

    rightbar

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <!-- header -->
-    <h1>Muslimbek</h1>
   </div>
 </template>
 
---
+++ components/RightSidebar.vue
@@ -0,0 +1,62 @@
+<template>
+
+    <div class="mt-[100px] ml-[500px]">
+        <!--  -->
+        <div class="flex w-[319px] h-[56px]">
+            <img class="rounded-full" src="../assets/images/Ppng.png" alt="">
+            <div class="ml-[18px] w-[209px] text-[14px] mt-[7px]">
+                <p class="font-bold">upvox_</p>
+                <p>Upvox</p>
+            </div>
+            <p class="text-[#0095F6] text-[12px] flex items-center">Switch</p>
+        </div>
+        <!--  -->
+        <!--  -->   
+        <div class="w-[351px] mt-[15px] ">
+            <div class="flex justify-between ml-[-4px] mr-[32px]"> 
+                <p class="text-[14px] text-[#8E8E8E]">Suggestions for you</p>
+                <p class="text-[11px] font-bold flex items-center">See All</p>
+            </div>
+            <div class="mt-[8px]">
+                <div class="flex items-center w-[319px] justify-between">
+                    <img class="rounded-full w-[32px] h-[32px]" src="../assets/images/man.png" alt="">
+                     <div class="ml-[18px] w-[209px] text-[14px] mt-[7px]">
+                        <p class="font-bold text-[14px]">imkir</p>
+                        <p class="text-[#8E8E8E] text-[12px]">Follows you</p>
+                     </div>
+                    <p class="text-[#0095F6] text-[12px] flex items-center font-bold">Follow</p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="w-[319px] text-[#C7C7C7] text-[12px] mt-[23px]">
+            <div class="flex gap-[3px] w-[319px]">
+                <p>About</p>
+                <p>Help</p>
+                <p>Press</p>
+                <p>API</p>
+                <p>Jobs</p>
+                <p>Privacy</p>
+                <p>Terms</p>
+                <p>Locations</p>
+            </div>
+
+            <div class="flex mt-[2px]">
+                <p>Language</p>
+                <p>Meta Verified</p>
+            </div>
+
+            <div class="mt-[21px]">
+                <p>© 2023 INSTAGRAM FROM META</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
